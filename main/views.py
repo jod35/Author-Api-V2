9e34e14bbc2094cea8302554ee5d9972f84d0d1d
jod35 <jodestrevin@gmail.com>
2020-07-28 06:34:13 -0700

    get all authors

--- main/views.py
+++ main/views.py
@@ -1,10 +1,30 @@
 from main import app,db
 from flask import request,jsonify,request,make_response
-from main.models import Author
+from main.models import Author,AuthorOuputSchema
 
 @app.route('/')
 def index():
     return jsonify({'message':"Hello Welcome to my API"})
+
+#############################################
+###########Getting all the authors###########
+#############################################
+@app.route('/authors',methods=['GET'])
+def get_all_authors():
+    #get all authors
+    get_authors=Author.query.all()
+
+    # create an instance of the ouput schema
+    author_schema=AuthorOuputSchema(many=True)
+
+    #serialize our SQLAlchemy objects basing on the fields we described using the output schema
+    authors=author_schema.dump(get_authors)
+
+    return make_response(jsonify(
+        {
+            "authors":authors
+        }
+    ))
 
 
 @app.errorhandler(404)
